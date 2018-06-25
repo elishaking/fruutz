1f3aefb6f79602841d318b26d8658717d7633451
elishaking <ek.chibueze@gmail.com>
2018-06-25 23:53:33 +0100

    model

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import * as tf from '@tensorflow/tfjs';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,7 +10,18 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    
+  }
 
+  ionViewDidEnter(){
+    tf.loadModel('assets/tfjs_model/fruits_2_5_30/model.json').then((fruits_model) => {
+      // console.log(fruits_model.toJSON());
+      let img = new Image(100, 100);
+      img.src = "assets/imgs/test_imgs/apricot.jpg";
+      const ex = tf.expandDims(tf.fromPixels(img));
+      const prediction = fruits_model.predict(ex);
+      console.log(prediction);
+    });
   }
 
 }
