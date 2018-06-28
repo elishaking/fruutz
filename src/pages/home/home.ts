651e5f79acd9d3cc4cdc5b65c22717bf019f175d
elishaking <ek.chibueze@gmail.com>
2018-06-28 14:52:52 +0100

    camera

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import * as tf from '@tensorflow/tfjs';
+import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -9,18 +10,31 @@ import * as tf from '@tensorflow/tfjs';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private camera: Camera) {
     
   }
 
   ionViewDidEnter(){
     tf.loadModel('assets/tfjs_model/fruits_2_5_30/model.json').then((fruits_model) => {
       // console.log(fruits_model.toJSON());
-      let img = new Image(100, 100);
+      let img = new Image(224, 224);
       img.src = "assets/imgs/test_imgs/apricot.jpg";
-      const ex = tf.expandDims(tf.fromPixels(img));
+      const ex = tf.cast(tf.expandDims(tf.fromPixels(img), 0), "float32");
+      // ex.reshape([1, 100, 100, 3]);
       const prediction = fruits_model.predict(ex);
-      console.log(prediction);
+      console.log(prediction.toString());
+    });
+  }
+
+  takePhoto(){
+    this.camera.getPicture({
+      allowEdit: true,
+      correctOrientation: false,
+      targetHeight: 100,
+      targetWidth: 100,
+      saveToPhotoAlbum: true,
+    }).then((image) => {
+      console.log(image);
     });
   }
 
