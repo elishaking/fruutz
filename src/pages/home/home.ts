b524bafa6d343928e89521bf2e2d609b84b42156
elishaking <ek.chibueze@gmail.com>
2018-07-04 08:39:35 +0100

    load model

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 
 import * as tf from '@tensorflow/tfjs';
 import { Camera } from '@ionic-native/camera';
@@ -9,32 +9,47 @@ import { Camera } from '@ionic-native/camera';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  prediction = "None";
 
-  constructor(public navCtrl: NavController, private camera: Camera) {
+  constructor(public navCtrl: NavController, private camera: Camera,
+    private loadingCtrl: LoadingController) {
     
   }
 
   ionViewDidEnter(){
-    tf.loadModel('assets/tfjs_model/fruits_2_5_30/model.json').then((fruits_model) => {
-      // console.log(fruits_model.toJSON());
-      let img = new Image(224, 224);
-      img.src = "assets/imgs/test_imgs/apricot.jpg";
-      const ex = tf.cast(tf.expandDims(tf.fromPixels(img), 0), "float32");
-      // ex.reshape([1, 100, 100, 3]);
-      const prediction = fruits_model.predict(ex);
-      console.log(prediction.toString());
-    });
+    
   }
 
   takePhoto(){
     this.camera.getPicture({
       allowEdit: true,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
       correctOrientation: false,
-      targetHeight: 100,
-      targetWidth: 100,
+      targetHeight: 224,
+      targetWidth: 224,
       saveToPhotoAlbum: true,
     }).then((image) => {
-      console.log(image);
+      // console.log(image);
+      this.predictFruit(image);
+    });
+  }
+
+  predictFruit(image){
+    let loading = this.loadingCtrl.create({
+      content: "Detecting Fruit"
+    });
+    loading.present();
+    tf.loadModel('assets/tfjs_model/fruits_2_5_30/model.json').then((fruits_model) => {
+      // console.log(fruits_model.toJSON());
+      let img = new Image(224, 224);
+      img.src = 'data:image/jpeg;base64,' + image;
+      const ex = tf.cast(tf.expandDims(tf.fromPixels(img), 0), "float32");
+      // ex.reshape([1, 100, 100, 3]);
+      const prediction = fruits_model.predict(ex);
+      this.prediction = prediction.toString();
+      loading.dismiss();
     });
   }
 
